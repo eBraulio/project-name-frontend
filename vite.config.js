d4ce57ae77dfd58c99c2400c179e3d9ce0d9b0d2
BraulioBanuelos <emmibb@gmail.com>
2024-12-25 23:34:17 -0600

    Iniciar desarrollo en rama secundaria

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import postcss from "@vituum/vite-plugin-postcss";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), postcss()],
+  //plugins: [react()],
+  server: {
+    port: 3000, // Cambia el número de puerto a 3000.
+    open: true,
+  },
+  //root: "./src",
+  //base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
