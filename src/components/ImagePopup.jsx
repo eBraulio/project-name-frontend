1d01e63ce93bc3dde38f96fda98eb2434b1326ca
BraulioBanuelos <emmibb@gmail.com>
2025-01-04 01:26:13 -0600

    Agregar Popup a albums con Overlay funcionando

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -1,9 +1,29 @@
+import React, { useEffect, useRef } from "react";
+
 export default function ImagePopup({ link, name, onClose, isOpen }) {
+  useEffect(() => {
+    // Manejador para la tecla Escape
+    const handleEscClose = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    // Añadir event listener para la tecla Escape
+    document.addEventListener("keydown", handleEscClose);
+
+    // Limpieza del event listener cuando el componente se desmonte
+    return () => {
+      document.removeEventListener("keydown", handleEscClose);
+    };
+  }, [onClose]);
+
   return (
     <div className={`popup  ${isOpen ? "popup__opened" : ""}`} id="image-popup">
       <div
         className={`popup__overlay ${isOpen ? "popup__overlay-opened" : ""}`}
         id="popup-overlay-image"
+        onClick={onClose}
       ></div>
       <div className="popup__content-image">
         <button
