79fe6c2640ce2830e9318be1a3283c9110563ce3
BraulioBanuelos <emmibb@gmail.com>
2025-01-06 23:04:28 -0600

    Aplicacion funcionando antes de re estructuracion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -240,6 +240,21 @@ function App() {
         <div className="Homepage__container">
           <div className="Homepage__body">
             <div className="Homepage__body-description">
+              <p className="Homepage__body-project">
+                This WEB site has been developed as the Final Project (Type 1 –
+                Frontend connected to an external API) of the Full-stack WEB
+                Developer bootcamp, by TripleTen.{" "}
+              </p>
+              <p className="Homepage__body-project">
+                The site has been built using React + Vite. The application is
+                deployed using Firebase with Google authentication; and
+                connected to the Spotify API. All the project is accessible via
+                GitHub.
+              </p>
+              <p className="Homepage__body-project">
+                Any feedback is greatly appreciated and can be sent via the
+                contact channels (Github/LinkedIn).
+              </p>
               <h2 className="Homepage__body-title">Let's try it!</h2>
               <button
                 onClick={handleGoogleLogin}
